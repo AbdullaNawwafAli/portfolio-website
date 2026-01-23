356df277aa1e9782dadd165dde1e05d840644ba0
AbdullaNawwafAli <nawwafali4x@gmail.com>
2026-01-23 23:15:27 +0500

    feat: remove db connection testing code feat: add in svg image to test working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
-import { getUsers } from "@/server/queries/users";
 import { Button } from "@/ui/shadcn/button";
+import Image from "next/image";
 
 export default async function Home() {
   // const users= await getUsers();
 
   return (
     <div>
-      <Button>
-        hello
-      </Button>
-      {/* <pre>{JSON.stringify(users, null, 2)}</pre> */}
+      <Button variant={"ghost"}>hello</Button>
+      <Button>do da</Button>
+      <div>
+        <Image src={'navBarSVG.svg'} alt = {'here'} width={175} height={50}/>
+      </div>
     </div>
   );
 }
