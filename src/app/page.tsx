5366798daf42b077ef2294b167649ba035232fae
AbdullaNawwafAli <nawwafali4x@gmail.com>
2026-01-23 21:22:23 +0500

    feat: comment out code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import { getUsers } from "@/server/queries/users";
 import { Button } from "@/ui/shadcn/button";
 
 export default async function Home() {
-  const users= await getUsers();
+  // const users= await getUsers();
 
   return (
     <div>
       <Button>
         hello
       </Button>
-      <pre>{JSON.stringify(users, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(users, null, 2)}</pre> */}
     </div>
   );
 }
