947ece1582f81f9dab9b8cec38d332bc8026669c
AbdullaNawwafAli <nawwafali4x@gmail.com>
2026-01-24 18:34:09 +0500

    feat: boiler plate / testing code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-import { Button } from "@/ui/shadcn/button";
-import Image from "next/image";
-
 export default async function Home() {
   // const users= await getUsers();
 
-  return (
-    <div>
-      <Button variant={"ghost"}>hello</Button>
-      <Button>do da</Button>
-      <div>
-        <Image src={'navBarSVG.svg'} alt = {'here'} width={175} height={50}/>
-      </div>
-    </div>
-  );
+  return <></>;
 }
