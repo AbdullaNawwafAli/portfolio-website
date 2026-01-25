4a147eef704adbe4cc626f2331a900bdb07815c7
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2026-01-25 11:10:31 +0500

    feat: transfer css from global.css to tailwind in navbar

--- src/ui/layout/Navbar.tsx
+++ src/ui/layout/Navbar.tsx
@@ -2,33 +2,35 @@ import Image from "next/image";
 import { Button } from "../shadcn/button";
 import { ralewayNav } from "@/app/layout";
 
+
+const navBarButtonStyle = "hidden md:flex box-border justify-end items-start gap-2.5 w-[129px] h-full flex-auto self-stretch grow"
+
 const Navbar = () => {
-  const keys = [...Array(5).keys()]
-
   const navBarButtons = [{ label: "Home", route: '/' }, { label: "Projects", route: '/' }, { label: "Hobbies", route: '/' }, { label: "About Me", route: '/' }]
+
   return (
-      <div className="navbar-container">
+    <div className="fixed top-0 left-0 w-full flex flex-row justify-start items-start p-[10px_30px] gap-2.5 z-1">
 
-        {/* Logo in invisible column*/}
-        <div className="navbar-logo">
-          <Image src={"navBarSVG.svg"} alt={"here"} width={175} height={50} />
-        </div>
-
-        {/*invisible columns to make sure buttons and navbar stay in position regardless of screen size*/}
-        {keys.map((key) => (
-          <div className="navbar-buttons" key={key}>
-          </div>
-        ))}
-
-        {/*buttons in invisible columns*/}
-        {navBarButtons.map((button) => (
-          <div className="navbar-buttons" key={button.label}>
-            <Button variant={"ghost"} className="px-0">
-              <span className={ralewayNav.className}>{button.label}</span>
-            </Button>
-          </div>
-        ))}
+      {/* Logo in invisible column*/}
+      <div className="box-border flex flex-row justify-start items-start gap-2.5 w-[20px] md:w-[129px] h-full flex-auto self-stretch grow">
+        <Image src={"navBarSVG.svg"} alt={"here"} width={200} height={50} style={{ minWidth: 175, objectFit:'contain'}} />
       </div>
+
+      {/*Invisible columns to make sure buttons and navbar stay in position regardless of screen size*/}
+      {[...Array(5).keys()].map((key) => (
+        <div className={navBarButtonStyle} key={key}>
+        </div>
+      ))}
+
+      {/*Buttons in invisible columns*/}
+      {navBarButtons.map((button) => (
+        <div className={navBarButtonStyle} key={button.label}>
+          <Button variant={"ghost"} className="px-0">
+            <span className={ralewayNav.className}>{button.label}</span>
+          </Button>
+        </div>
+      ))}
+    </div>
   );
 };
 
