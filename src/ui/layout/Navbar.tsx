025fa6b8e7d9f1dff91c2d809ec62bda6386ca4d
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2026-01-25 09:50:27 +0500

    feat: revamp navbar to change according to resolution

--- src/ui/layout/Navbar.tsx
+++ src/ui/layout/Navbar.tsx
@@ -1,38 +1,34 @@
 import Image from "next/image";
 import { Button } from "../shadcn/button";
-import {ralewayNav } from "@/app/layout";
+import { ralewayNav } from "@/app/layout";
 
 const Navbar = () => {
+  const keys = [...Array(5).keys()]
+
+  const navBarButtons = [{ label: "Home", route: '/' }, { label: "Projects", route: '/' }, { label: "Hobbies", route: '/' }, { label: "About Me", route: '/' }]
   return (
-    <>
-      <div className="flex w-full justify-between px-[30px] py-2 items-center">
-        <div>
+      <div className="navbar-container">
+
+        {/* Logo in invisible column*/}
+        <div className="navbar-logo">
           <Image src={"navBarSVG.svg"} alt={"here"} width={175} height={50} />
         </div>
-        <div className="hidden md:flex gap-[20px]">
-          <div className="flex w-[129px] justify-end">
+
+        {/*invisible columns to make sure buttons and navbar stay in position regardless of screen size*/}
+        {keys.map((key) => (
+          <div className="navbar-buttons" key={key}>
+          </div>
+        ))}
+
+        {/*buttons in invisible columns*/}
+        {navBarButtons.map((button) => (
+          <div className="navbar-buttons" key={button.label}>
             <Button variant={"ghost"} className="px-0">
-              <span className={ralewayNav.className}>Home</span>
+              <span className={ralewayNav.className}>{button.label}</span>
             </Button>
           </div>
-          <div className="flex w-[129px] justify-end">
-            <Button variant={"ghost"} className="px-0">
-              <span className={ralewayNav.className}>Projects</span>
-            </Button>
-          </div>
-          <div className="flex w-[129px] justify-end">
-            <Button variant={"ghost"} className="px-0">
-              <span className={ralewayNav.className}>Hobbies</span>
-            </Button>
-          </div>
-          <div className="flex w-[129px] justify-end">
-            <Button variant={"ghost"} className="px-0">
-              <span className={ralewayNav.className}>About Me</span>
-            </Button>
-          </div>
-        </div>
+        ))}
       </div>
-    </>
   );
 };
 
