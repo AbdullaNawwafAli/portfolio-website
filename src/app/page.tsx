95fe9fbac439e609fc94eb496f7e732930fc4210
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2026-01-26 12:03:35 +0500

    feat: install shadcn card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Hero from "@/ui/Hero";
 import { Button } from "@/ui/shadcn/button";
+import { Card, CardContent } from "@/ui/shadcn/card";
+import Image from "next/image";
 
 export default async function Home() {
 
@@ -8,6 +10,11 @@ export default async function Home() {
     <div>
       <Hero />
       <Button>Click Me</Button>
+      <div className="flex justify-between gap-2.5  ">
+        {[...Array(5).keys()].map((key) => (
+          <Card key={key} className="w-full " ><CardContent><Image src={"navBarSVG.svg"} alt={"Website logo/text"} width={175} height={100} style={{ minWidth: 175, objectFit: 'contain' }} /></CardContent> </Card>
+        ))}</div>
+
     </div>
   </>;
 }
