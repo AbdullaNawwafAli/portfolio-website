3f7e03ec33559b1d7262f798dee774d331d101b9
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2026-01-26 12:03:53 +0500

    feat: change text sizes of hero

--- src/ui/Hero.tsx
+++ src/ui/Hero.tsx
@@ -1,24 +1,24 @@
 
 const Hero = () => {
     return (
-        <div className="flex flex-row items-end justify-between">
+        <div className="flex flex-col md:flex-row items-end justify-between">
 
-            <div className="flex flex-col items-start justify-start  h-full">
+            <div className="flex flex-col items-start justify-start h-full">
                 <span
-                    className=" font-heading font-bold text-[128px] leading-[120px] text-[#243C4C]
-     max-md:text-[96px] max-md:leading-[90px]"
+                    className=" font-heading font-bold text-9xl leading-[120px] text-[#243C4C]
+     max-md:text-7xl max-md:leading-[90px]"
                 >
                     De me Omnia
                 </span>
-                <span className=" font-sans font-semibold text-2xl  text-[#243C4C]">
+                <span className=" font-sans font-semibold text-xl  text-[#243C4C]">
                     A Latin Phrase translating to &apos;All Things About Me&apos;
                 </span>
             </div>
-            <div className="flex flex-col items-end justify-end h-full">
-                <span className=" font-sans font-semibold text  text-[#243C4C] ">
+            <div className="flex flex-col items-end justify-end text-xl h-full">
+                <span className=" font-sans font-bold text  text-[#243C4C] ">
                     A Portfolio Website
                 </span>
-                <span className="font-sans font-semibold text-2xl  text-[#243C4C] ">
+                <span className="font-sans font-semibold text-xl  text-[#243C4C] ">
                     By Ryfaem, a Software Developer
                 </span>
             </div>
