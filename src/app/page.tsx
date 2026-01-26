6aaf13b975a11bbb8122da55ca9c82c95039647a
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2026-01-26 09:59:23 +0500

    feat: create new Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
+import Hero from "@/features/Hero/components/Hero";
 import { Button } from "@/ui/shadcn/button";
 
 export default async function Home() {
-  // const users= await getUsers();
+
 
   return <>
     <div>
-      <div className='hero'>
-        De me Omnia
-      </div>
+      <Hero />
       <Button>Click Me</Button>
     </div>
   </>;
---
+++ src/features/Hero/components/Hero.tsx
@@ -0,0 +1,13 @@
+
+const Hero = () => {
+    return (
+        <div
+            className="flex items-center font-heading font-bold text-[128px] leading-[120px] text-[#243C4C]
+         max-md:text-[96px] max-md:leading-[90px]"
+        >
+            De me Omnia
+        </div>
+    )
+}
+
+export default Hero
