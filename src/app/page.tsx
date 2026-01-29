9d22839561727d4f3f1df26a747a6b46aa9e21fb
AbdullaNawwafAli <nawwafali4x@gmail.com>
2026-01-29 21:17:06 +0500

    feat: remove cloudinary image from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { getProjects } from "@/server/queries/projects";
 import Cards from "@/ui/Cards";
-import CloudinaryImage from "@/ui/CloudinaryImage";
 import Hero from "@/ui/Hero";
 
 export default async function Home() {
@@ -12,12 +11,6 @@ export default async function Home() {
       <div className="flex flex-col gap-2 flex-1 h-full justify-between py-10 w-full">
         <Hero />
         <Cards />
-        <CloudinaryImage
-          src={getProj[0].media[0].cloudinaryId}
-          alt="here"
-          width={400}
-          height={400}
-        />
       </div>
     </>
   );
