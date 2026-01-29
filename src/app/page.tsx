cf4afd2230f81298a76de87b8b89552baa0dc305
AbdullaNawwafAli <nawwafali4x@gmail.com>
2026-01-29 18:24:39 +0500

    feat: use cloudinary image to show image in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
+import { getProjects } from "@/server/queries/projects";
 import Cards from "@/ui/Cards";
+import CloudinaryImage from "@/ui/CloudinaryImage";
 import Hero from "@/ui/Hero";
 
 export default async function Home() {
+  const getProj = await getProjects();
+  console.log(getProj);
+
   return (
     <>
       <div className="flex flex-col gap-2 flex-1 h-full justify-between py-10 w-full">
         <Hero />
         <Cards />
+        <CloudinaryImage
+          src={getProj[0].media[0].cloudinaryId}
+          alt="here"
+          width={400}
+          height={400}
+        />
       </div>
     </>
   );
