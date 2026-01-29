68ba2901c944bdb332a75d65b0904c2a10558c8e
AbdullaNawwafAli <nawwafali4x@gmail.com>
2026-01-29 18:23:36 +0500

    feat: update drizzle db connection to send schema with connection string

--- src/server/db/drizzle.ts
+++ src/server/db/drizzle.ts
@@ -1,4 +1,12 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as schema from "./drizzle/schema";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+console.log(Object(schema));
+
+export const db = drizzle(pool, { schema });
