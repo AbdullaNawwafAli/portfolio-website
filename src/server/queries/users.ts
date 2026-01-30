55cdc7a879704f6a2d3541ef6d1cc078e84600ec
AbdullaNawwafAli <nawwafali4x@gmail.com>
2026-01-30 15:59:16 +0500

    feat: delete users schema and users queries

--- src/server/queries/users.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { db } from "@/server/db/drizzle";
-import { usersTable } from "../db/drizzle/schema/users";
-
-export async function getUsers() {
-  const users = await db.select().from(usersTable);
-  return users;
-}
