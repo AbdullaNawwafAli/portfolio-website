8b881c8211576a821ceba3b23ab451ad27ced73d
AbdullaNawwafAli <nawwafali4x@gmail.com>
2026-02-04 07:33:12 +0500

    feat: add headers for login page

--- src/app/(public)/admin-login/page.tsx
+++ src/app/(public)/admin-login/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const AdminLogin = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <div className="flex flex-col gap-2 flex-1 h-full justify-between py-10 w-full">
+        <div className="flex flex-col">
+          <div className="text-8xl flex items-center justify-center">
+            STOP!! WHO GOES THERE?
+          </div>
+          <div className="text-5xl flex justify-center center-text">
+            IDENTIFY THYSELF! WHAT&apos;S THE SECRET PHRASE?
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AdminLogin;
