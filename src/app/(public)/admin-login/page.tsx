044b55c33e8472eb215bda4b1d8dd091b45fe808
AbdullaNawwafAli <nawwafali4x@gmail.com>
2026-02-06 11:32:39 +0500

    feat: add input shadcn component

--- src/app/(public)/admin-login/page.tsx
+++ src/app/(public)/admin-login/page.tsx
@@ -1,4 +1,13 @@
-import React from "react";
+import {
+  FieldSet,
+  FieldLegend,
+  FieldDescription,
+  FieldGroup,
+  Field,
+  FieldLabel,
+  FieldError,
+} from "@/ui/shadcn/field";
+import { Input } from "@/ui/shadcn/input";
 
 const AdminLogin = () => {
   return (
@@ -11,6 +20,20 @@ const AdminLogin = () => {
           <div className="text-5xl flex justify-center center-text">
             IDENTIFY THYSELF! WHAT&apos;S THE SECRET PHRASE?
           </div>
+          <FieldSet>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="Name">Name</FieldLabel>
+                <Input id="Name" autoComplete="off" />
+                <FieldError>DON&apos;T KNOWS YOU.</FieldError>
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="Secret">Secret Phrase</FieldLabel>
+                <Input id="name" autoComplete="off" placeholder="Evil Rabbit" />
+                <FieldError>BOUT TO GET STABBED.</FieldError>
+              </Field>
+            </FieldGroup>
+          </FieldSet>
         </div>
       </div>
     </>
