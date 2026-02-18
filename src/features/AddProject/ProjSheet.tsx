35da557004fc7427cc79c576cd08286a0c62f416
AbdullaNawwafAli <nawwafali4x@gmail.com>
2026-02-18 13:18:40 +0500

    feat: create base structure for projsheet

--- src/features/AddProject/ProjSheet.tsx
+++ src/features/AddProject/ProjSheet.tsx
@@ -1,12 +1,13 @@
-import { Button } from "@/ui/shadcn/button";
+"use client"
+import { Button } from "@/ui/shadcn/button"
 import {
   FieldGroup,
   Field,
   FieldLabel,
   FieldDescription,
   FieldError,
-} from "@/ui/shadcn/field";
-import { Input } from "@/ui/shadcn/input";
+} from "@/ui/shadcn/field"
+import { Input } from "@/ui/shadcn/input"
 import {
   Sheet,
   SheetClose,
@@ -16,69 +17,103 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from "@/ui/shadcn/sheet";
-import { SquarePen } from "lucide-react";
-import { useForm } from "@tanstack/react-form";
-import { toast } from "sonner";
-import * as z from "zod";
+} from "@/ui/shadcn/sheet"
+import { SquarePen } from "lucide-react"
+import { useForm } from "@tanstack/react-form"
+import { toast } from "sonner"
+import * as z from "zod"
+import {
+  InputGroup,
+  InputGroupTextarea,
+  InputGroupAddon,
+  InputGroupText,
+} from "@/ui/shadcn/input-group"
 
 const formSchema = z.object({
-  title: z
+  name: z
     .string()
-    .min(5, "Bug title must be at least 5 characters.")
-    .max(32, "Bug title must be at most 32 characters."),
-  description: z
+    .min(5, "Name must be at least 5 characters.")
+    .max(32, "Name must be at most 32 characters."),
+  name_subtext: z
     .string()
-    .min(20, "Description must be at least 20 characters.")
-    .max(100, "Description must be at most 100 characters."),
-});
+    .min(5, "Name subtext must be at least 5 characters.")
+    .max(32, "Name subtext must be at most 32 characters."),
+  hero_description: z
+    .string()
+    .min(20, "Hero description must be at least 20 characters.")
+    .max(100, "Hero description must be at most 100 characters."),
+  email: z.email(),
+  instagram_url: z
+    .string()
+    .min(1, "Instagram URL must be at least 1 character."),
+  linked_in_url: z
+    .string()
+    .min(1, "LinkedIn URL must be at least 1 character."),
+  github_url: z.string().min(1, "GitHub URL must be at least 1 character."),
+  resume_pdf_cloudinary_id: z
+    .string()
+    .min(1, "Resume PDF Cloudinary ID must be at least 1 character."),
+})
 
 export function SheetSide() {
   const form = useForm({
     defaultValues: {
-      title: "",
-      description: "",
+      name: "",
+      hero_description: "",
     },
     validators: {
-      onSubmit: formSchema,
+      onChange: formSchema,
     },
     onSubmit: async ({ value }) => {
-      toast.success("Form submitted successfully");
+      toast("You submitted the following values:", {
+        description: (
+          <pre className="bg-code text-code-foreground mt-2 w-[320px] overflow-x-auto rounded-md p-4">
+            <code>{JSON.stringify(value, null, 2)}</code>
+          </pre>
+        ),
+        position: "bottom-right",
+        classNames: {
+          content: "flex flex-col gap-2",
+        },
+        style: {
+          "--border-radius": "calc(var(--radius)  + 4px)",
+        } as React.CSSProperties,
+      })
     },
-  });
-
+  })
   return (
     <div className="flex flex-wrap gap-2">
       <Sheet>
-        <SheetTrigger asChild>
-          <Button variant="ghost" className="capitalize font-sans">
-            EDIT <SquarePen />
-          </Button>
-        </SheetTrigger>
-        <SheetContent
-          side={"right"}
-          className="data-[side=bottom]:max-h-[50vh] data-[side=top]:max-h-[50vh]"
+        <form
+          id="bug-report-form"
+          onSubmit={(e) => {
+            e.preventDefault()
+            form.handleSubmit()
+          }}
         >
-          <SheetHeader>
-            <SheetTitle>Edit profile</SheetTitle>
-            <SheetDescription>
-              Make changes to your profile here. Click save when you&apos;re
-              done.
-            </SheetDescription>
-          </SheetHeader>
-          <div className="no-scrollbar overflow-y-auto px-4">
-            <form
-              onSubmit={(e) => {
-                e.preventDefault();
-                form.handleSubmit();
-              }}
-            >
+          <SheetTrigger asChild>
+            <Button variant="ghost" className="capitalize font-sans">
+              EDIT <SquarePen />
+            </Button>
+          </SheetTrigger>
+          <SheetContent
+            side={"right"}
+            className="data-[side=bottom]:max-h-[50vh] data-[side=top]:max-h-[50vh]"
+          >
+            <SheetHeader>
+              <SheetTitle>Edit Bio</SheetTitle>
+              <SheetDescription>
+                Make changes to your Bio here. Click save when you&apos;re done.
+                You cannot change the profile picture or edit the work, skills
+                and education data here.
+              </SheetDescription>
+            </SheetHeader>
+            <div className="no-scrollbar overflow-y-auto px-4">
               <FieldGroup>
-                <form.Field
-                  name="title"
-                  children={(field) => {
+                <form.Field name="name">
+                  {(field) => {
                     const isInvalid =
-                      field.state.meta.isTouched && !field.state.meta.isValid;
+                      field.state.meta.isTouched && !field.state.meta.isValid
                     return (
                       <Field data-invalid={isInvalid}>
                         <FieldLabel htmlFor={field.name}>Bug Title</FieldLabel>
@@ -92,28 +127,69 @@ export function SheetSide() {
                           placeholder="Login button not working on mobile"
                           autoComplete="off"
                         />
-                        <FieldDescription>
-                          Provide a concise title for your bug report.
-                        </FieldDescription>
                         {isInvalid && (
                           <FieldError errors={field.state.meta.errors} />
                         )}
                       </Field>
-                    );
+                    )
                   }}
-                />
+                </form.Field>
+                <form.Field name="hero_description">
+                  {(field) => {
+                    const isInvalid =
+                      field.state.meta.isTouched && !field.state.meta.isValid
+                    return (
+                      <Field data-invalid={isInvalid}>
+                        <FieldLabel htmlFor={field.name}>
+                          Description
+                        </FieldLabel>
+                        <InputGroup>
+                          <InputGroupTextarea
+                            id={field.name}
+                            name={field.name}
+                            value={field.state.value}
+                            onBlur={field.handleBlur}
+                            onChange={(e) => field.handleChange(e.target.value)}
+                            placeholder="I'm having an issue with the login button on mobile."
+                            rows={6}
+                            className="min-h-24 resize-none"
+                            aria-invalid={isInvalid}
+                          />
+                          <InputGroupAddon align="block-end">
+                            <InputGroupText className="tabular-nums">
+                              {field.state.value.length}/100 characters
+                            </InputGroupText>
+                          </InputGroupAddon>
+                        </InputGroup>
+                        {isInvalid && (
+                          <FieldError errors={field.state.meta.errors} />
+                        )}
+                      </Field>
+                    )
+                  }}
+                </form.Field>
               </FieldGroup>
-              <Button type="submit">Submit</Button>
-            </form>
-          </div>
-          <SheetFooter>
-            <Button type="submit">Save changes</Button>
-            <SheetClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </SheetClose>
-          </SheetFooter>
-        </SheetContent>
+            </div>
+            <SheetFooter>
+              <Button type="submit" form="bug-report-form" className="w-full">
+                Submit
+              </Button>
+
+              <Button
+                type="button"
+                variant="outline"
+                onClick={() => form.reset()}
+                className="w-full"
+              >
+                Reset
+              </Button>
+              <SheetClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </SheetClose>
+            </SheetFooter>
+          </SheetContent>
+        </form>
       </Sheet>
     </div>
-  );
+  )
 }
