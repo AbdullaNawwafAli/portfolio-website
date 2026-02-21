738edea218a7c978971f33dda8d7219d17acf510
AbdullaNawwafAli <nawwafali4x@gmail.com>
2026-02-21 18:09:40 +0500

    feat: use tanstack query to fetch bio data

--- src/app/(secure)/admin-home/page.tsx
+++ src/app/(secure)/admin-home/page.tsx
@@ -1,11 +1,14 @@
+"use client"
 import AdminHero from "@/features/Hero/components/AdminHero"
-import { getBioApi } from "@/lib/api-calls/bio"
+import createBioQueryOptions from "@/lib/TanstackQueries/createBioQueryOptions"
+import { useQuery } from "@tanstack/react-query"
 
-const AdminHomePage = async () => {
-  const bioData = await getBioApi()
+const AdminHomePage = () => {
+  const { data, isPending } = useQuery(createBioQueryOptions())
+
   return (
     <div className="flex flex-col gap-2 flex-1 h-full justify-between py-10 w-full">
-      <AdminHero data={bioData} />
+      <AdminHero data={data} />
     </div>
   )
 }
