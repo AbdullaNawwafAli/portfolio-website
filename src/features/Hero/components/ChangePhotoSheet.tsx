ec499bcaff40f09ba7b35cac3eedb068b529b06e
AbdullaNawwafAli <nawwafali4x@gmail.com>
2026-02-28 11:21:44 +0500

    chore: update imports

--- src/features/Hero/components/ChangePhotoSheet.tsx
+++ src/features/Hero/components/ChangePhotoSheet.tsx
@@ -17,10 +17,8 @@ import { changeHeroPhotoSchema } from "../lib/zod/changeHeroPhotoSchema"
 import { useAppForm } from "@/features/TanstackForm/hooks"
 import { toast } from "sonner"
 import { convertToBase64 } from "@/lib/utils/fileUtils"
-import {
-  OverWriteFileToCloudinaryApi,
-  OverWriteFileToCloudinaryApiType,
-} from "@/lib/api-calls/cloudinary"
+import { OverWriteFileToCloudinaryApi } from "@/lib/api-calls/cloudinary"
+import { OverWriteFileToCloudinaryApiType } from "@/types/cloudinaryData"
 
 interface ChangeHeroPhotoSheetProps {
   bio_picture_cloudinary_id: string
