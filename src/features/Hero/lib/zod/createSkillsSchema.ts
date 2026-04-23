f349bd177ba217068b0efe42c8dd489deedb9358
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2026-04-23 15:23:12 +0500

    feat: create schema for skills creation

--- src/features/Hero/lib/zod/createSkillsSchema.ts
+++ src/features/Hero/lib/zod/createSkillsSchema.ts
@@ -2,8 +2,12 @@ import z from "zod"
 
 export const createSkillsSchema = z.object({
   skill_type_name: z.string().min(1, "Name must be at least 1 characters."),
-  hero_photo: z.instanceof(File, {
-    message: "Hero photo is required",
-  }),
-  responsibilities: z.union([z.array(z.string()), z.undefined()]),
+  skills: z
+    .array(
+      z.object({
+        skill_name: z.string().min(1, "Skill name is required."),
+        skill_logo_cloudinary_id: z.string(),
+      })
+    )
+    .min(1, "Add at least one skill."),
 })
