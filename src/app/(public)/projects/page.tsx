87daadca3884e0c174bdc6fc1bf19527e1f9219c
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2026-05-17 11:05:42 +0500

    chore: remove unused functionality

--- src/app/(public)/projects/page.tsx
+++ src/app/(public)/projects/page.tsx
@@ -1,16 +1,13 @@
-import { NewProjectSheet } from "@/features/Projects/components/NewProjectSheet"
-
-const Projects = () => {
+const ProjectsPage = () => {
   return (
     <div className="h-full w-full flex-1">
       <div className="h-full w-full flex gap-2 items-center justify-start py-10">
         <span className="font-heading text-3xl font-semibold text-primary">
           My Projects
         </span>
-        <NewProjectSheet />
       </div>
     </div>
   )
 }
 
-export default Projects
+export default ProjectsPage
