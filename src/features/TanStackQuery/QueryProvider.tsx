bc134f283c62bfe3e9a62f3b1d243d3ba0d4882a
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2026-05-18 14:52:13 +0500

    feat: add in react query tools

--- src/features/TanStackQuery/QueryProvider.tsx
+++ src/features/TanStackQuery/QueryProvider.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import React, { useState } from "react"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 interface QueryProviderProps {
   children: React.ReactNode
@@ -11,7 +12,10 @@ const QueryProvider = ({ children }: QueryProviderProps) => {
   const [queryClient] = useState(() => new QueryClient())
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
