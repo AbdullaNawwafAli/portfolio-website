6fab6eabf0d90fb34620c7b20a2ad94b23721f8e
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2026-05-18 08:55:14 +0500

    feat: add in cards component to adminprojects page

--- src/app/(secure)/admin-projects/page.tsx
+++ src/app/(secure)/admin-projects/page.tsx
@@ -1,16 +1,19 @@
+import Cards from "@/features/Projects/components/Cards"
 import { Button } from "@/ui/shadcn/button"
 import Link from "next/link"
-import React from "react"
 
 const AdminProjectsPage = () => {
   return (
     <div className="flex flex-col gap-2 flex-1 h-full justify-between py-10 w-full">
       <div className="flex justify-between">
-        <span>Projects</span>
+        <span className="font-heading text-3xl font-semibold text-primary">
+          My Projects
+        </span>
         <Link href={"/admin-projects/new-project"}>
           <Button className="capitalize font-sans">New Project</Button>
         </Link>
       </div>
+      <Cards />
     </div>
   )
 }
