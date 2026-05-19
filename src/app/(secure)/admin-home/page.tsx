d002c90fb80bd21fa15bce4d3501b533c3c481b8
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2026-05-19 11:14:52 +0500

    feat: add in hydration boundary to admin home page for ssr and prefetching

--- src/app/(secure)/admin-home/page.tsx
+++ src/app/(secure)/admin-home/page.tsx
@@ -4,6 +4,7 @@ import { getBioApi } from "@/lib/api-calls/bio"
 import { getEducationApi } from "@/lib/api-calls/education"
 import { getSkillsApi } from "@/lib/api-calls/skills"
 import { getWorkApi } from "@/lib/api-calls/work"
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
 
 const AdminHomePage = async () => {
   const queryClient = getQueryClient()
@@ -29,9 +30,11 @@ const AdminHomePage = async () => {
   })
 
   return (
-    <div className="flex flex-col gap-2 flex-1 h-full justify-between py-10 w-full">
-      <AdminHero />
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="flex flex-col gap-2 flex-1 h-full justify-between py-10 w-full">
+        <AdminHero />
+      </div>
+    </HydrationBoundary>
   )
 }
 
