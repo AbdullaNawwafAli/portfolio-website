b8ce62746706c7cac33f0fbb01141bdd1609fc13
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2026-05-20 08:19:19 +0500

    feat: change cards to use project cards and change to a grid system

--- src/features/Projects/components/Cards.tsx
+++ src/features/Projects/components/Cards.tsx
@@ -19,9 +19,12 @@ import {
   EmptyTitle,
 } from "@/ui/shadcn/empty"
 import { Folder } from "lucide-react"
+import ProjectCard from "./ProjectCard"
 
 const Cards = () => {
-  const { data, isPending } = useQuery(createProjectsQueryOptions())
+  const { data, isPending } = useQuery(
+    createProjectsQueryOptions({ staleTime: 1000 * 60 * 5 })
+  )
   console.log(data)
 
   if (data?.length === 0) {
@@ -42,39 +45,9 @@ const Cards = () => {
   }
 
   return (
-    <div className="flex justify-between gap-2.5 h-[400px] items-end ">
-      {[...Array(5).keys()].map((key) => (
-        <Card
-          key={key}
-          className="w-full flex align-center bg-transparent border-0 gap-0 shadow-none group"
-        >
-          <CardHeader className="flex justify-between px-1 py-0">
-            <CardTitle className="font-sans font-semibold text-md text-center items-end">
-              0{key + 1}
-            </CardTitle>
-            <CardTitle className="font-sans font-semibold text-md text-center">
-              Software Project
-            </CardTitle>
-          </CardHeader>
-          <div className="w-full rounded-t-sm  overflow-hidden">
-            <Image
-              src={"/stock.jpg"}
-              alt={"Website logo/text"}
-              width={200}
-              height={200}
-              style={{ width: "100%", height: "auto" }}
-            />
-          </div>
-          <CardFooter className="flex overflow-hidden rounded-b-sm max-h-0 bg-primary group-hover:max-h-16 opacity-0 group-hover:opacity-100 transition-all duration-500 ease-in-out justify-between items-center px-1 py-0">
-            <span className="font-sans text-background">21 April 2025</span>
-            <Button
-              variant={"ghost"}
-              className="px-0 font-sans hover:bg-transparent hover:text-background text-background"
-            >
-              View
-            </Button>
-          </CardFooter>
-        </Card>
+    <div className="grid grid-cols-2 md:grid-cols-5 gap-2.5">
+      {data?.map((project) => (
+        <ProjectCard data={project} key={project.id} />
       ))}
     </div>
   )
