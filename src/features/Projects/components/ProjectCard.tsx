fff489d19748b0f8aa9112d1872e4f4faa8efbf9
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2026-05-20 08:19:46 +0500

    feat: change tailwind to make every image same size and also use cloudinary image instead of next image

--- src/features/Projects/components/ProjectCard.tsx
+++ src/features/Projects/components/ProjectCard.tsx
@@ -1,7 +1,7 @@
 import { ProjectData } from "@/types/projectData"
+import CloudinaryImage from "@/ui/CloudinaryImage"
 import { Button } from "@/ui/shadcn/button"
 import { Card, CardFooter, CardHeader, CardTitle } from "@/ui/shadcn/card"
-import Image from "next/image"
 
 interface ProjectCardProps {
   data: ProjectData
@@ -9,7 +9,7 @@ interface ProjectCardProps {
 
 const ProjectCard = ({ data }: ProjectCardProps) => {
   return (
-    <Card className="w-full flex align-center bg-transparent border-0 gap-0 shadow-none group">
+    <Card className="w-full bg-transparent border-0 gap-0 shadow-none group">
       <CardHeader className="flex justify-between px-1 py-0">
         <CardTitle className="font-sans font-semibold text-md text-center items-end">
           0
@@ -18,13 +18,15 @@ const ProjectCard = ({ data }: ProjectCardProps) => {
           {data.name}
         </CardTitle>
       </CardHeader>
-      <div className="w-full rounded-t-sm  overflow-hidden">
-        <Image
-          src={"/stock.jpg"}
-          alt={"Website logo/text"}
-          width={200}
-          height={200}
-          style={{ width: "100%", height: "auto" }}
+      <div className="rounded-t-sm overflow-hidden aspect-4/3 w-full">
+        {" "}
+        {/* aspect ratio keeps all images same height */}
+        <CloudinaryImage
+          alt={data?.media?.[0].alt as string}
+          src={data?.media?.[0].cloudinaryId as string}
+          width={400}
+          height={400}
+          className="w-full h-full object-cover"
         />
       </div>
       <CardFooter className="flex overflow-hidden rounded-b-sm max-h-0 bg-primary group-hover:max-h-16 opacity-0 group-hover:opacity-100 transition-all duration-500 ease-in-out justify-between items-center px-1 py-0">
