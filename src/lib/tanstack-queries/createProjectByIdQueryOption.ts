5dcf7a95a4dad842afeae58d951414ad9b6cd852
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2026-05-20 13:22:48 +0500

    feat: create new tanstack query for fetching data by id

---
+++ src/lib/tanstack-queries/createProjectByIdQueryOption.ts
@@ -0,0 +1,24 @@
+import { ProjectData } from "@/types/projectData"
+import { queryOptions, UseQueryOptions } from "@tanstack/react-query"
+import { getProjectByIdApi } from "../api-calls/projects"
+
+interface params {
+  projectId: string
+}
+
+export default function createProjectByIdQueryOption<
+  TData = ProjectData,
+  TError = Error,
+>(
+  params?: params,
+  options?: Omit<
+    UseQueryOptions<ProjectData, TError, TData>,
+    "queryKey" | "queryFn"
+  >
+) {
+  return queryOptions({
+    ...options,
+    queryKey: ["project", params],
+    queryFn: () => getProjectByIdApi(params?.projectId as string),
+  })
+}
