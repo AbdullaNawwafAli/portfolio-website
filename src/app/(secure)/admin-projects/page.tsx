39b80c2924e000768681565fcb5f1a5fae7e7d9c
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2026-05-20 08:18:56 +0500

    feat: add in prefetching and remove the justify between tailwind

--- src/app/(secure)/admin-projects/page.tsx
+++ src/app/(secure)/admin-projects/page.tsx
@@ -1,20 +1,30 @@
 import Cards from "@/features/Projects/components/Cards"
+import { getQueryClient } from "@/features/TanStackQuery/getQueryClient"
+import createProjectsQueryOptions from "@/lib/tanstack-queries/createProjectsQueryOptions"
 import { Button } from "@/ui/shadcn/button"
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
 import Link from "next/link"
 
-const AdminProjectsPage = () => {
+const AdminProjectsPage = async () => {
+  const queryClient = getQueryClient()
+
+  await queryClient.prefetchQuery(
+    createProjectsQueryOptions({ staleTime: 1000 * 60 * 5 })
+  )
   return (
-    <div className="flex flex-col gap-2 flex-1 h-full justify-between py-10 w-full">
-      <div className="flex justify-between">
-        <span className="font-heading text-3xl font-semibold text-primary">
-          My Projects
-        </span>
-        <Link href={"/admin-projects/new-project"}>
-          <Button className="capitalize font-sans">New Project</Button>
-        </Link>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="flex flex-col gap-2 flex-1 h-full py-10 w-full">
+        <div className="flex justify-between">
+          <span className="font-heading text-3xl font-semibold text-primary">
+            My Projects
+          </span>
+          <Link href={"/admin-projects/new-project"}>
+            <Button className="capitalize font-sans">New Project</Button>
+          </Link>
+        </div>
+        <Cards />
       </div>
-      <Cards />
-    </div>
+    </HydrationBoundary>
   )
 }
 
