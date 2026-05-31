dbccff95b534374d486ee15ca206300fc95de263
AbdullaNawwafAli <nawwaf.ali4x@gmail.com>
2026-05-31 10:46:12 +0500

    chore: remove redundant imports

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next"
 import Navbar from "@/features/Shell/components/Navbar"
 import Footer from "@/features/Shell/components/Footer"
-import { cinzel, raleway } from "@/lib/fonts"
-import QueryProvider from "@/features/TanStackQuery/components/QueryProvider"
-import { Toaster } from "@/ui/shadcn/sonner"
 
 export const metadata: Metadata = {
   title: "Nawwaf's Portfolio Website",
